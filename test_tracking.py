88a2cf5ded0ca461fd22a2a7e20e748f4ef513b9
Gabriel Vieira <gluisvieira@gmail.com>
2018-04-09 15:18:40 -0300

    Add WIP test script

--- test_tracking.py
+++ test_tracking.py
@@ -4,10 +4,27 @@ from tracking import *
 
 class TestTracking(unittest.TestCase):
   def test_empty_frames(self):
-    frames = []
+    frames = [
+      {'boxes': []},
+      {'boxes': []},
+      {'boxes': []}
+    ]
     tracker = Tracker(frames)
     self.assertEqual(len(tracker.tracks), 0)
     self.assertEqual(len(tracker.humans), 0)
+  
+  def test_single_frame(self):
+    frames = [
+      {'boxes': [], 'humans': []},
+      {'boxes': []},
+      {'boxes': [
+        [5, 10, 25, 40]
+      ]}
+      tracker = Tracker(frames)
+      self.assertEqual(len(tracker.tracks), 1)
+      self.assertEqual(len(tracker.humans), 1)
+    ]
+  
 
 if __name__ == '__main__':
   unittest.main()
