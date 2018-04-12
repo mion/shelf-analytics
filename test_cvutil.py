9e5b7f5bc13dc70e410610890a00c9d0811f4c84
Gabriel Vieira <gluisvieira@gmail.com>
2018-04-12 16:31:15 -0300

    Test drawing bbox on a frame

--- test_cvutil.py
+++ test_cvutil.py
@@ -10,9 +10,19 @@ if __name__ == '__main__':
   if not video.isOpened():
       print("ERROR: could not open video")
       sys.exit()
+
   frames = cvutil.read_frames_from_video(video)
   if frames == None:
       print('ERROR: could not read frames from video file')
       sys.exit()
-  else:
-      print("Done, loaded " + str(len(frames)) + " frames")
+  print("Done, loaded " + str(len(frames)) + " frames")
+
+  frame = frames[0]
+  width = video.get(3)
+  height = video.get(4)
+  print("Width: {0}\nHeight: {1}".format(str(width), str(height)))
+  # frame_with_rect = cv2.rectangle(frame, (0, 0), (100, 125), (255, 50, 10), 2)
+  new_frame = cvutil.draw_bbox_on_frame(frame, (10, 15, 60, 120))
+  cv2.imshow('frame', new_frame)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
