dfd1e96aff0cde1b47265c9c02ca591e498a2dd4
Gabriel Vieira <gluisvieira@gmail.com>
2018-04-13 16:03:14 -0300

    Fix export all tracks bug

--- visualize_tracks.py
+++ visualize_tracks.py
@@ -1,8 +1,10 @@
+import os
 import sys
 import argparse
 import json
 import cv2
 import cvutil
+import tnt
 
 def load_json(path):
   with open(path) as json_file:
@@ -26,16 +28,27 @@ if __name__ == '__main__':
 
   tags = load_json(args.tags_path)
   tracks = load_json(args.tracks_path)
-  track = tracks[args.track_index]
-  track_first_index = 0
-  track_last_index = 18
+  track_index = args.track_index
+  print("Exporting track {0}...".format(track_index))
+  track = tracks[track_index]
+  track_first_index = track[0]["index"]
+  track_last_index = track[len(track) - 1]["index"]
+
+  bundle_path = "/Users/gvieira/temp/tagged_frames/video-01-d-fps-5/tracks"
+  track_folder_path = os.path.join(bundle_path, "track-{0}".format(track_index))
+  try:
+    os.mkdir(track_folder_path)
+  except FileExistsError as err:
+    print(tnt.color_warn("ERROR: could not create directory at {0}".format(track_folder_path)))
+    print(err)
 
   frames = cvutil.read_frames_from_video(video)
+
   for i in range(len(frames)):
     frame = frames[i]
     for obj_detected_bbox in tags["frames"][i]["boxes"]:
       cvutil.draw_bbox_on_frame(frame, obj_detected_bbox, rect_color=(0,0,255), text_color=(255,255,255))
     
     if i >= track_first_index and i <= track_last_index:
-      cvutil.draw_bbox_on_frame(frame, track[i]["bbox"], rect_color=(255, 155, 0), text_color=(0,155,255))
-      cvutil.save_image(frame, "track-{0}-frame-{1}.png".format(args.track_index, i), "/Users/gvieira/temp/tagged_frames/video-01-d-fps-5/tracks")
+      cvutil.draw_bbox_on_frame(frame, track[i - track_first_index]["bbox"], rect_color=(255, 155, 0), text_color=(0,155,255))
+      cvutil.save_image(frame, "track-{0}-frame-{1}.png".format(track_index, i), track_folder_path)
