36fa9f7eff44ed9e1f5f5207d7932dad7a820648
Gabriel Vieira <gluisvieira@gmail.com>
2018-04-18 17:43:30 -0300

    Add WIP main shelf script

--- shan.py
+++ shan.py
@@ -1,7 +1,26 @@
+#  Copyright 2018 Toneto Labs. All Rights Reserved.
+#
+"""Main Shelf Analytics script."""
 import os
+import argparse
 import json
 
-def load_tagged_bundle(path):
-  tags_file_path = os.path.join(path, "tags.json")
-  with open(tags_file_path, "r") as tags_file:
-    return json.loads(tags_file.read())
+from colorize import red, green, yellow, header
+
+
+def load_json(path):
+    with open(path, "r") as tags_file:
+      return json.loads(tags_file.read())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("video_file_path", help="path to a video file")
+    parser.add_argument(
+        "rois_file_path", help="path to the Regions of Interest JSON file")
+    parser.add_argument("output_dir_path", help="path to the output directory")
+    args = parser.parse_args()
+
+    print("Video file:", args.video_file_path, sep=" ")
+    print("Regions of Interest JSON file:", args.rois_file_path, sep=" ")
+    print("Output directory:", args.output_dir_path, sep=" ")
