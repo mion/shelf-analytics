4189c67c10a2dcb031dbc91fe1929dc2c6153143
Gabriel Vieira <gluisvieira@gmail.com>
2018-05-03 15:19:43 -0300

    Refactor split_frames.py: do not create directory anymore

--- split_frames.py
+++ split_frames.py
@@ -13,16 +13,7 @@ from tnt import extract_video_name, has_ffmpeg_installed, DEFAULT_TRANSCODED_VID
 
 def split_frames(input_path, output_dir, ext):
   frame_number_format = '%07d' # FIXME count total number of frames beforehand
-  _, input_name = os.path.split(input_path)
-  name, _ = os.path.splitext(input_name)
-  video_dir = os.path.join(output_dir, name)
-  try:
-    os.mkdir(video_dir)
-  except FileExistsError as err:
-    print(yellow("ERROR: could not create directory at {0}".format(video_dir)))
-    print(err)
-    return False
-  output_path = os.path.join(video_dir, "frame-{0}.{1}".format(frame_number_format, ext))
+  output_path = os.path.join(output_dir, "frame-{0}.{1}".format(frame_number_format, ext))
   cmd_template = "ffmpeg -i {0} {1} -hide_banner"
   # [!] WARNING: shell=True is dangerous
   cmd = cmd_template.format(input_path, output_path)
