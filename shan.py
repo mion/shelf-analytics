356a724c62d8a4624b10ffa7e2753a99e4fd60da
Gabriel Vieira <gluisvieira@gmail.com>
2018-05-08 10:55:16 -0300

    Clean shan script

--- shan.py
+++ shan.py
@@ -14,13 +14,4 @@ def load_json(path):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("video_file_path", help="path to a video file")
-    parser.add_argument(
-        "rois_file_path", help="path to the Regions of Interest JSON file")
-    parser.add_argument("output_dir_path", help="path to the output directory")
-    args = parser.parse_args()
-
-    print("Video file:", args.video_file_path, sep=" ")
-    print("Regions of Interest JSON file:", args.rois_file_path, sep=" ")
-    print("Output directory:", args.output_dir_path, sep=" ")
+    print("Hello")
