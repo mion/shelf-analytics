aee2e7d24e14c33d3e2d367f2b8a62a5f99084b5
Gabriel Vieira <gluisvieira@gmail.com>
2018-05-14 04:09:27 -0300

    Fix split_frames script

--- scripts/split_frames.py
+++ scripts/split_frames.py
@@ -2,10 +2,14 @@
 #
 """Split a video file into many images."""
 
+import os
+import sys
+sys.path.append(os.path.join(os.getcwd(), 'shan'))
+sys.path.append(os.path.join(os.getcwd(), 'shan/mask_rcnn'))
+
 import argparse
 import subprocess
 import shutil
-import os
 
 from colorize import red, green, yellow, header
 from tnt import extract_video_name, has_ffmpeg_installed, DEFAULT_TRANSCODED_VIDEO_NAME
@@ -17,7 +21,6 @@ def split_frames(input_path, output_dir, ext):
   cmd_template = "ffmpeg -i {0} {1} -hide_banner"
   # [!] WARNING: shell=True is dangerous
   cmd = cmd_template.format(input_path, output_path)
-  print("\n\n\t" + cmd + "\n\n")
   result = subprocess.call(cmd, shell=True)
   return result == 0
 
@@ -30,13 +33,9 @@ if __name__ == "__main__":
   args = parser.parse_args()
 
   if has_ffmpeg_installed():
-    print(yellow("Input video: ") + args.input_path)
-    print(yellow("Output directory: ") + args.output_dir)
-    print(yellow("Frame images extension: ") + str(args.ext))
-    print("Working...")
     success = split_frames(args.input_path, args.output_dir, args.ext)
     if success:
-      print(green("Done!"))
+      print(green("DONE! Image files created in output directory."))
     else:
       print(red("ERROR: failed to split video into frames"))
   else:
