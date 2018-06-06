624fee1d740c36ff4d2498555178cbcdb3d6a525
Gabriel Vieira <gluisvieira@gmail.com>
2018-06-06 13:55:13 -0300

    Add improved track_objects2 script

---
+++ scripts/track_objects2.py
@@ -0,0 +1,27 @@
+import os
+import sys
+sys.path.append(os.path.join(os.getcwd(), 'shan'))
+sys.path.append(os.path.join(os.getcwd(), 'shan/mask_rcnn'))
+
+import json
+import argparse
+from tnt import load_json, load_frames, load_bboxes_per_frame
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("video_path", help="path to the video")
+    parser.add_argument("tags_path", help="path to a tags JSON file")
+    parser.add_argument("output_dir_path", help="path to the output directory where a JSON file with the tracks will be saved")
+    args = parser.parse_args()
+
+    print("Loading video...")
+    frames = load_frames(args.video_path)
+    if frames == None:
+        print('ERROR: could not read frames from video file')
+        sys.exit()
+    else:
+        print("Done, loaded " + str(len(frames)) + " frames")
+    
+    print("Loading tags...")
+    tags = load_json(args.tags_path)
+    bboxes_per_frame = load_bboxes_per_frame(tags)
