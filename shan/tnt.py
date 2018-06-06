7d8f34bea922790076783b50a57c479292f20e98
Gabriel Vieira <gluisvieira@gmail.com>
2018-06-06 13:54:35 -0300

    Improve tnt helper module

--- shan/tnt.py
+++ shan/tnt.py
@@ -5,23 +5,43 @@
 import os
 import shutil
 import json
+import cv2
+import cvutil
 
 DEFAULT_TRANSCODED_VIDEO_NAME = "video"
 
 def has_ffmpeg_installed():
-  return shutil.which("ffmpeg") != None
+    return shutil.which("ffmpeg") != None
 
 def extract_video_name(path):
-  return os.path.basename(os.path.normpath(path))
+    return os.path.basename(os.path.normpath(path))
 
 def extract_video_fps(dir):
-  return int(extract_video_name(dir).split("-")[-1])
+    return int(extract_video_name(dir).split("-")[-1])
 
 def add_suffix_to_basename(path, suffix):
-  base_path, base_name = os.path.split(os.path.normpath(path))
-  name, ext = os.path.splitext(base_name)
-  return os.path.join(base_path, name + suffix + ext)
+    base_path, base_name = os.path.split(os.path.normpath(path))
+    name, ext = os.path.splitext(base_name)
+    return os.path.join(base_path, name + suffix + ext)
 
 def load_json(path):
-  with open(path, "r") as tags_file:
-    return json.loads(tags_file.read())
+    with open(path, "r") as tags_file:
+        return json.loads(tags_file.read())
+
+def load_frames(path):
+    video = cv2.VideoCapture(path)
+    if not video.isOpened():
+        return None
+    return cvutil.read_frames_from_video(video)
+
+def load_bboxes_per_frame(tags):
+    bboxes_per_frame = []
+    for i in range(len(tags["frames"])):
+        bboxes_per_frame[i] = []
+        for bbox in tags["frames"][i]["boxes"]:
+            # IMPORTANT: OpenCV expects tuples, not lists.
+            #            Also, tuples will be used as indexes so
+            #            ints are necessary.
+            bboxes_per_frame[i].append(tuple([int(n) for n in bbox]))
+    return bboxes_per_frame
+
