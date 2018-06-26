f2f54a5442d0e07eaeacf348f78a202132ebd3ce
Gabriel Vieira <gluisvieira@gmail.com>
2018-06-26 13:37:23 -0300

    Update aux scripts

--- scripts/track_objs.sh
+++ scripts/track_objs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 TARGET_DIR=/tmp
 rm $TARGET_DIR/tracks.json
-python scripts/track_objects.py ~/shan/video-33-p_06/video-33-p_06-fps-10.mp4 ~/shan/video-33-p_06/frames_tagged/tags.json $TARGET_DIR
+python scripts/track_objects.py ~/shan/video-33-p_06/video-33-p_06-fps-10.mp4 ~/shan/video-33-p_06/frames_retagged/tags.json $TARGET_DIR
--- scripts/viz_tracks.sh
+++ scripts/viz_tracks.sh
@@ -2,4 +2,4 @@
 TARGET_DIR=/tmp
 rm -rf $TARGET_DIR/tracks
 mkdir $TARGET_DIR/tracks
-python scripts/visualize_tracks2.py ~/shan/video-33-p_06/video-33-p_06-fps-10.mp4 ~/shan/video-33-p_06/frames_tagged/tags.json /tmp/tracks.json $TARGET_DIR/tracks
+python scripts/visualize_tracks2.py ~/shan/video-33-p_06/video-33-p_06-fps-10.mp4 ~/shan/video-33-p_06/frames_retagged/tags.json /tmp/tracks.json $TARGET_DIR/tracks
