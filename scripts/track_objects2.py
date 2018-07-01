0043f6379b445b42a0507de66aa0a37f746df0e7
Gabriel Vieira <gluisvieira@gmail.com>
2018-07-01 18:06:51 -0300

    Load frame bundles

--- scripts/track_objects2.py
+++ scripts/track_objects2.py
@@ -1,3 +1,5 @@
+import pdb
+
 import os
 import sys
 sys.path.append(os.path.join(os.getcwd(), 'shan'))
@@ -7,6 +9,7 @@ import json
 import argparse
 from tnt import load_json, load_frames, load_bboxes_per_frame
 from tracking2 import HumanTracker
+from frame_bundle import load_frame_bundles
 
 cfg = load_json('shan/calibration-config.json')
 
@@ -16,16 +19,13 @@ if __name__ == '__main__':
     parser.add_argument('tags_path', help='path to a tags JSON file')
     parser.add_argument('output_dir_path', help='path to the output directory where a JSON file with the tracks will be saved')
     args = parser.parse_args()
-
-    print('Loading video...')
-    frames = load_frames(args.video_path)
-    if frames == None:
-        print('ERROR: could not read frames from video file')
-        sys.exit()
     
     print('Loading tags...')
     tags = load_json(args.tags_path)
-    bboxes_per_frame = load_bboxes_per_frame(tags)
+
+    print('Loading frame bundles from video...')
+    frame_bundles = load_frame_bundles(args.video_path, tags)
+    pdb.set_trace()
 
     print('Tracking humans...')
     
---
+++ shan/frame_bundle.py
@@ -0,0 +1,33 @@
+from cv2 import VideoCapture
+from cvutil import read_frames_from_video
+from bounding_box import BoundingBox as BBox, BoundingBoxFormat as BBoxFormat
+
+def load_frames(video_path):
+    video = VideoCapture(video_path)
+    if not video.isOpened():
+        return None
+    return read_frames_from_video(video)
+
+def load_frame_bundles(video_path, tags):
+    frames = load_frames(video_path)
+    frame_bundles = []
+    if len(frames) != len(tags['frames']):
+        raise RuntimeError('frames array mismatch between tags JSON and original video file')
+    for raw_obj in tags['frames']:
+        idx = int(raw_obj['frame_index'])
+        frame_bundles.append(FrameBundle(frames[idx], raw_obj['frame_index'], raw_obj['boxes'], raw_obj['scores']))
+    return frame_bundles
+    
+class FrameBundle:
+    """
+    A frame bundle is simply a container for these things:
+        - The frame image data loaded with OpenCV.
+        - The frame index in the original video.
+        - The bounding boxes inside it.
+        - The probability of each bounding box being a human.
+    """
+    def __init__(self, frame, raw_frame_index, raw_bboxes, raw_scores):
+        self.frame = frame
+        self.frame_index = int(raw_frame_index)
+        self.bboxes = [BBox(raw_bbox, BBoxFormat.y1_x1_y2_x2) for raw_bbox in raw_bboxes]
+        self.scores = [float(raw_score) for raw_score in raw_scores]
