97a0fbf9306c9efdbb2765c24c90c6a646dc1a2f
Gabriel Vieira <gluisvieira@gmail.com>
2018-07-01 17:25:24 -0300

    Add calib config

--- scripts/track_objects2.py
+++ scripts/track_objects2.py
@@ -8,27 +8,24 @@ import argparse
 from tnt import load_json, load_frames, load_bboxes_per_frame
 from tracking2 import HumanTracker
 
-
-DEFAULT_MAX_TRACKS = 100
+cfg = load_json('shan/calibration-config.json')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("video_path", help="path to the video")
-    parser.add_argument("tags_path", help="path to a tags JSON file")
-    parser.add_argument("output_dir_path", help="path to the output directory where a JSON file with the tracks will be saved")
+    parser.add_argument('video_path', help='path to the video')
+    parser.add_argument('tags_path', help='path to a tags JSON file')
+    parser.add_argument('output_dir_path', help='path to the output directory where a JSON file with the tracks will be saved')
     args = parser.parse_args()
 
-    print("Loading video...")
+    print('Loading video...')
     frames = load_frames(args.video_path)
     if frames == None:
         print('ERROR: could not read frames from video file')
         sys.exit()
-    else:
-        print("Done, loaded " + str(len(frames)) + " frames")
     
-    print("Loading tags...")
+    print('Loading tags...')
     tags = load_json(args.tags_path)
     bboxes_per_frame = load_bboxes_per_frame(tags)
 
-    print("Tracking humans...")
+    print('Tracking humans...')
     
