be65bb492dc7a7b82468d5f62080d40684157331
Gabriel Vieira <gluisvieira@gmail.com>
2018-07-01 17:13:15 -0300

    Change script dir

--- scripts/track_objs.sh
+++ scripts/track_objs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-TARGET_DIR=/tmp
+TARGET_DIR=/Users/gvieira/shan/test
 rm $TARGET_DIR/tracks.json
 python scripts/track_objects.py ~/shan/video-33-p_06/video-33-p_06-fps-10.mp4 ~/shan/video-33-p_06/frames_retagged/tags.json $TARGET_DIR
