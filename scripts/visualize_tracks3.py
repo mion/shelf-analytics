04f8540489b9c50a35b08d50d89036a11baafe15
Gabriel Vieira <gluisvieira@gmail.com>
2018-07-05 16:49:05 -0300

    Draw bbox and transition

--- scripts/visualize_tracks3.py
+++ scripts/visualize_tracks3.py
@@ -8,6 +8,20 @@ import numpy as np
 import cv2
 
 from tnt import load_json, load_frames
+from bounding_box import BoundingBox as BBox, BoundingBoxFormat as BBoxFormat
+from tracking2 import Transition
+from drawing import draw_bbox_outline, draw_bbox_line_between_centers
+
+AVAILABLE_BBOX_OUTLINE_COLOR = (192, 192, 192)
+AVAILABLE_BBOX_OUTLINE_THICKNESS = 1
+FILTERED_BBOX_OUTLINE_COLOR = (128, 128, 128)
+FILTERED_BBOX_OUTLINE_THICKNESS = 1
+DESELECTED_TRACKED_BBOX_OUTLINE_COLOR = (255, 255, 255)
+DESELECTED_TRACKED_BBOX_OUTLINE_THICKNESS = 2
+SELECTED_TRACKED_BBOX_OUTLINE_COLOR = (250, 150, 0)
+SELECTED_TRACKED_BBOX_OUTLINE_THICKNESS = 2
+TRANSITION_TRACKED_BBOX_OUTLINE_COLOR = (0, 150, 250)
+TRANSITION_TRACKED_BBOX_OUTLINE_THICKNESS = 1
 
 class TrackingVisualizationTool:
     def __init__(self, frames, tracking_result):
@@ -16,13 +30,48 @@ class TrackingVisualizationTool:
         # the render function uses the `state` variable to
         # draw the image to be displayed, and nothing else
         self.state = {}
-        self.state['track_id'] = 0
+        self.state['track_id'] = 1
         self.state['frame_index'] = 0
         self.state['footer_view'] = 1
     
+    def render_bboxes(self, frame, bboxes, transition_by_bbox_track_ids):
+        for bbox in bboxes:
+            if bbox.is_available():
+                frame = draw_bbox_outline(frame, bbox, AVAILABLE_BBOX_OUTLINE_COLOR, AVAILABLE_BBOX_OUTLINE_THICKNESS)
+            elif bbox.is_filtered():
+                frame = draw_bbox_outline(frame, bbox, FILTERED_BBOX_OUTLINE_COLOR, FILTERED_BBOX_OUTLINE_THICKNESS)
+            else:
+                if bbox.is_child_of(self.state['track_id']):
+                    # transition
+                    key = (bbox.id, self.state['track_id'])
+                    if key not in transition_by_bbox_track_ids:
+                        raise RuntimeError('pair (bboxID, selected trackID) not found in transition_by_bbox_track_ids where bboxID={0}, trackID={1}'.format(str(bbox.id), str(self.state['track_id'])))
+                    transition = transition_by_bbox_track_ids[key]
+                    frame = draw_bbox_outline(frame, transition.orig_bbox, TRANSITION_TRACKED_BBOX_OUTLINE_COLOR, TRANSITION_TRACKED_BBOX_OUTLINE_THICKNESS)
+                    frame = draw_bbox_line_between_centers(frame, transition.orig_bbox, bbox, TRANSITION_TRACKED_BBOX_OUTLINE_COLOR, TRANSITION_TRACKED_BBOX_OUTLINE_THICKNESS)
+                    # actual bbox
+                    frame = draw_bbox_outline(frame, bbox, SELECTED_TRACKED_BBOX_OUTLINE_COLOR, SELECTED_TRACKED_BBOX_OUTLINE_THICKNESS)
+                else:
+                    frame = draw_bbox_outline(frame, bbox, DESELECTED_TRACKED_BBOX_OUTLINE_COLOR, DESELECTED_TRACKED_BBOX_OUTLINE_THICKNESS)
+        return frame
+    
     def render(self):
-        # img = np.zeros((300, 512, 3), np.uint8)
-        return self.frames[self.state['frame_index']]
+        frame = self.frames[self.state['frame_index']]
+        # begintest
+        past_bbox = BBox([90, 90, 300, 200], BBoxFormat.x1_y1_w_h)
+        past_bbox.id = 120
+        past_bbox.parent_track_ids.append(1)
+        bboxes = [
+            BBox([100, 100, 300, 200], BBoxFormat.x1_y1_w_h)    
+        ]
+        bboxes[0].id = 123
+        bboxes[0].parent_track_ids.append(1)
+        transition_by_bbox_track_ids = {
+            (123, 1): Transition('snapped', past_bbox, past_bbox.distance_to(bboxes[0]))
+        }
+        # endtest
+        frame = self.render_bboxes(frame, bboxes, transition_by_bbox_track_ids)
+        return frame
     
     def on_change_frame_index(self, new_value):
         self.state['frame_index'] = new_value
--- shan/drawing.py
+++ shan/drawing.py
@@ -7,27 +7,13 @@ cfg = load_json('shan/calibration-config.json')
 
 TRANSITION_COLOR = (0, 155, 255)
 
-def draw_transition_line(frame, orig_bbox, dest_bbox, color=(0, 155, 255)):
-    cv2.line(frame, orig_bbox.center, dest_bbox.center, color, 1)
+def draw_bbox_line_between_centers(frame, orig_bbox, dest_bbox, color=(255, 255, 255), thickness=1):
+    cv2.line(frame, orig_bbox.center, dest_bbox.center, color, thickness)
     return frame
 
-def draw_text(frame, text, orig, color=(255, 255, 255)):
-  cv2.putText(frame, text, orig, cv2.FONT_HERSHEY_DUPLEX, 0.50, color, 1, cv2.LINE_AA)
-  return frame
-
-def draw_line(frame, orig, dest, color=(255, 255, 255), thickness=1):
-    cv2.line(frame, orig, dest, color, thickness, cv2.LINE_AA)
-    return frame
-
-def draw_rect(frame, bbox, color=(255, 255, 255), thickness=1):
+def draw_bbox_outline(frame, bbox, color=(255, 255, 255), thickness=1):
     cv2.rectangle(frame, (bbox.x1, bbox.y1), (bbox.x2, bbox.y2), color, thickness)
     return frame
-
-def draw_footer(frame, height):
-    frame_height, frame_width, frame_channels = frame.shape
-    frame_with_footer = np.zeros((frame_height + height, frame_width, frame_channels), np.uint8)
-    frame_with_footer[0:(frame_height - 1), 0:(frame_width - 1)] = frame[0:(frame_height - 1), 0:(frame_width - 1)]
-    return frame_with_footer
 
 def draw_calibration_config(frame, cfg):
     frame_height, frame_width, _ = frame.shape
@@ -43,3 +29,17 @@ def draw_calibration_config(frame, cfg):
         frame = draw_line(frame, (x + padding, y + 2 * padding), (x + padding + value, y + 2 * padding), thickness=2)
         x += column_width
     return frame
+
+def draw_text(frame, text, orig, color=(255, 255, 255)):
+  cv2.putText(frame, text, orig, cv2.FONT_HERSHEY_DUPLEX, 0.50, color, 1, cv2.LINE_AA)
+  return frame
+
+def draw_line(frame, orig, dest, color=(255, 255, 255), thickness=1):
+    cv2.line(frame, orig, dest, color, thickness, cv2.LINE_AA)
+    return frame
+
+def draw_footer(frame, height):
+    frame_height, frame_width, frame_channels = frame.shape
+    frame_with_footer = np.zeros((frame_height + height, frame_width, frame_channels), np.uint8)
+    frame_with_footer[0:(frame_height - 1), 0:(frame_width - 1)] = frame[0:(frame_height - 1), 0:(frame_width - 1)]
+    return frame_with_footer
