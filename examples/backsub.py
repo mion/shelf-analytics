80c225f2b2048217040e862f8b061c6be3737be4
Gabriel Vieira <gluisvieira@gmail.com>
2018-07-15 20:20:51 -0300

    Add background sub example

---
+++ examples/backsub.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture('/Users/gvieira/code/toneto/shan/test/videos/video-33-p_06.mp4')
+fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
+
+while(1):
+    ret, frame = cap.read()
+    fgmask = fgbg.apply(frame)
+    cv.imshow('frame',fgmask)
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv.destroyAllWindows()
