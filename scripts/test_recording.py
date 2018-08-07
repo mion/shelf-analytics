d29a36be45b19e58e38089a70fa0008f735cbf77
Gabriel Vieira <gluisvieira@gmail.com>
2018-08-07 20:47:45 -0300

    Improve test recording script

--- scripts/test_recording.py
+++ scripts/test_recording.py
@@ -1,14 +1,30 @@
+import argparse
 import time
 import numpy as np
 import cv2
 
+parser = argparse.ArgumentParser(description='Record a video to test the camera.')
+parser.add_argument('ext', help="file extension")
+parser.add_argument('codec', help="fourcc codec")
+parser.add_argument('fps', type=int, help="frames per second")
+parser.add_argument('size', help="a string like 640x480 or default")
+args = parser.parse_args()
+
 cap = cv2.VideoCapture(0)
-w = int(cap.get(3))
-h = int(cap.get(4))
-print('Default resolution: {}x{}'.format(w, h))
+w = None
+h = None
+if args.size == 'default':
+    w = int(cap.get(3))
+    h = int(cap.get(4))
+else:
+    w, h = map(int, args.size.split('x'))
+print('Extension: ', args.ext)
+print('Resolution: {}x{}'.format(w, h))
+print('Codec: ', args.codec)
+print('FPS: ', args.fps)
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 timestr = time.strftime('%Y-%m-%d-%H-%M-%S-Z%z', time.localtime())
-out = cv2.VideoWriter('test-recording-{}.mov'.format(timestr),fourcc, 24, (w,h), True)
+out = cv2.VideoWriter('test-recording-{}.{}'.format(timestr, args.ext),fourcc, 24, (w,h), True)
 # NOTE To write an image frame note that the imgFrame must be the same size as capSize above or updates will fail.
 while(cap.isOpened()):
     ret, frame = cap.read()
