59482e500ded8d1644cc74e16265b45ff1d17f15
Gabriel Vieira <gluisvieira@gmail.com>
2018-08-07 21:09:31 -0300

    Fix recording script

--- scripts/test_recording.py
+++ scripts/test_recording.py
@@ -6,25 +6,31 @@ import cv2
 parser = argparse.ArgumentParser(description='Record a video to test the camera.')
 parser.add_argument('ext', help="file extension")
 parser.add_argument('codec', help="fourcc codec")
-parser.add_argument('fps', type=int, help="frames per second")
-parser.add_argument('size', help="a string like 640x480 or default")
+parser.add_argument('fps', help="frames per second or 'source' to match video source")
+parser.add_argument('size', help="a string like 640x480 or 'source' to match video source (WARNING: if you get this wrong the recording will be empty)")
 args = parser.parse_args()
 
 cap = cv2.VideoCapture(0)
 w = None
 h = None
-if args.size == 'default':
-    w = int(cap.get(3))
-    h = int(cap.get(4))
+if args.size == 'source':
+    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 else:
     w, h = map(int, args.size.split('x'))
+
+if args.fps == 'source':
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+else:
+    fps = int(args.fps)
+
 print('Extension: ', args.ext)
 print('Resolution: {}x{}'.format(w, h))
 print('Codec: ', args.codec)
-print('FPS: ', args.fps)
+print('FPS: ', fps)
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 timestr = time.strftime('%Y-%m-%d-%H-%M-%S-Z%z', time.localtime())
-out = cv2.VideoWriter('test-recording-{}.{}'.format(timestr, args.ext),fourcc, 24, (w,h), True)
+out = cv2.VideoWriter('test-recording-{}.{}'.format(timestr, args.ext),fourcc, fps, (w,h), True)
 # NOTE To write an image frame note that the imgFrame must be the same size as capSize above or updates will fail.
 while(cap.isOpened()):
     ret, frame = cap.read()
