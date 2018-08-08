01907fc506f60215890930449afc9c5f51a43316
Gabriel Vieira <gluisvieira@gmail.com>
2018-08-08 09:13:02 -0300

    Clean script

--- bin/transplit.sh
+++ bin/transplit.sh
@@ -41,37 +41,3 @@ mkdir $OUTPUT_DIR_PATH/frames/raw
 # Transcode, then split frames
 python $TRANSCODE_SCRIPT_PATH --fps $FPS $INPUT_VIDEO_PATH $OUTPUT_VIDEO_PATH
 python $SPLIT_FRAMES_SCRIPT_PATH --ext $OUTPUT_FRAMES_EXT $OUTPUT_VIDEO_PATH $OUTPUT_DIR_PATH/frames/raw
-
-# (ai) ➜  shan git:(master) ✗ python scripts/split_frames.py -h
-# usage: split_frames.py [-h] [--ext EXT] input_video_path output_dir_path
-
-# positional arguments:
-#   input_video_path  input video file path
-#   output_dir_path   output directory
-
-# optional arguments:
-#   -h, --help        show this help message and exit
-#   --ext EXT         frame image file extension: 'png' (default) or 'jpg'
-
-# (ai) ➜  shan git:(master) ✗ python scripts/transcode.py -h
-# usage: transcode.py [-h] [--fps FPS] input_video_path output_video_path
-
-# positional arguments:
-#   input_video_path   input video file path
-#   output_video_path  output video file path
-
-# optional arguments:
-#   -h, --help         show this help message and exit
-#   --fps FPS          frames per second for output video (default is 10)
-# (ai) ➜  shan git:(master) ✗ bin/transplit.sh ./test/videos/video-01-p.mp4
-
-# CODE_PATH=~/code/toneto/shan
-# CALIB_PATH=$CODE_PATH/test/calib-configs/venue-11-shelf-1-fps-10.json
-# TAGS_PATH=$VIDEO_PATH/data/tags.json
-# OUTPUT_PATH=$VIDEO_PATH/data
-
-# if [ -z "$2" ]; then
-#     python scripts/track_objects2.py $VIDEO_FILE_PATH $CALIB_PATH $TAGS_PATH $OUTPUT_PATH
-# elif [ -n "$2" ]; then
-#     python -m pdb scripts/track_objects2.py $VIDEO_FILE_PATH $CALIB_PATH $TAGS_PATH $OUTPUT_PATH
-# fi
