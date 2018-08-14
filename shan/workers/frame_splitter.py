9c847f4f9781fc916b9f859017e9b6f1fd4eb7ea
Gabriel Vieira <gluisvieira@gmail.com>
2018-08-14 11:54:00 -0300

    Refactor framesplitter to use Worker class

--- shan/workers/frame_splitter.py
+++ shan/workers/frame_splitter.py
@@ -15,54 +15,27 @@ import pika
 import time
 import argparse
 from frame_splitting import split_frames
+from worker import Worker
 
-FRAME_SPLITTER_QUEUE_HOST = 'localhost'
-FRAME_SPLITTER_QUEUE_NAME = 'frame_splitter'
-FRAME_SPLITTER_QUEUE_DURABLE = True
-FRAME_SPLITTER_QUEUE_PREFETCH_COUNT = 1 # do not give more than one message to a worker at a time
-FRAME_SPLITTER_DELIVERY_MODE = 2 # make message persistent, for stronger guarantee of persistance see: https://www.rabbitmq.com/confirms.html
-
-def process_job(channel, method, properties, message):
-    job = json.loads(message)
-    print("Received job:")
-    print(job)
-    print("Processing...")
-    success = split_frames(job['input_video_path'], job['output_dir_path'], job['ext'])
-    if success:
-        print("Success!")
-    else:
-        print("Failed to process job")
-    channel.basic_ack(delivery_tag = method.delivery_tag)
-
-def add_job(input_video_path, output_video_path, fps):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=FRAME_SPLITTER_QUEUE_HOST))
-    channel = connection.channel()
-    channel.queue_declare(queue=FRAME_SPLITTER_QUEUE_NAME, durable=FRAME_SPLITTER_QUEUE_DURABLE)
-    job = {
-        'input_video_path': input_video_path,
-        'output_dir_path': output_video_path,
-        'ext': fps
-    }
-    message = json.dumps(job)
-    channel.basic_publish(exchange='',
-                        routing_key=FRAME_SPLITTER_QUEUE_NAME,
-                        body=message,
-                        properties=pika.BasicProperties(
-                            delivery_mode = FRAME_SPLITTER_DELIVERY_MODE, 
-                        ))
-    print('Job added to queue:')
-    print(job)
-    connection.close()
-
-def start_worker():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=FRAME_SPLITTER_QUEUE_HOST))
-    channel = connection.channel()
-    channel.queue_declare(queue=FRAME_SPLITTER_QUEUE_NAME, durable=FRAME_SPLITTER_QUEUE_DURABLE)
-    channel.basic_qos(prefetch_count=FRAME_SPLITTER_QUEUE_PREFETCH_COUNT) 
-    channel.basic_consume(process_job,
-                        queue=FRAME_SPLITTER_QUEUE_NAME)
-    print('Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
+class FrameSplitter(Worker):
+    def __init__(self):
+        conf = {
+            'QUEUE_HOST': 'localhost',
+            'QUEUE_NAME': 'frame_splitter_dev_1',
+            'QUEUE_DURABLE': True,
+            'QUEUE_PREFETCH_COUNT': 1, # do not give more than one message to a worker at a time
+            'DELIVERY_MODE': 2 # make message persistent, for stronger guarantee of persistance see: https://www.rabbitmq.com/confirms.html
+        }
+        super().__init__(conf)
+    
+    def process(self, job):
+        missing_keys = self.missing_keys(job, ['input_video_path', 'output_dir_path', 'ext'])
+        if len(missing_keys) > 0:
+            print("FAILURE: missing required keys '{}' in job JSON".format(missing_keys))
+            return
+        success = split_frames(job['input_video_path'], job['output_dir_path'], job['ext'])
+        if not success:
+            print('FAILURE: could not split frames from input video')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Frame splitter worker.')
@@ -73,6 +46,12 @@ if __name__ == '__main__':
     args = parser.parse_args()
     cmd = args.command
     if cmd == 'start':
-        start_worker()
+        worker = FrameSplitter()
+        worker.start()
     elif cmd == 'add':
-        add_job(args.input, args.output, args.ext)
+        worker = FrameSplitter()
+        worker.add_job({
+            'input_video_path': args.input, 
+            'output_dir_path': args.output, 
+            'ext': args.ext
+        })
