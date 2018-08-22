e2f11c09baf79cf59d6c498cf0a3a53306a1b7e3
Gabriel Vieira <gluisvieira@gmail.com>
2018-08-22 16:49:06 -0300

    Refactor print_events script

--- scripts/print_events.py
+++ scripts/print_events.py
@@ -41,20 +41,12 @@ def render_frame_with_events(frame, index, rois, bboxes_in_frame, events_in_fram
         curr_y += text_height + LINE_SPACE
     return frame
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('video_path', help='path to the video file')
-    parser.add_argument('rois_path', help='path to the rois JSON file')
-    parser.add_argument('tracking_result_path', help='path to a tracking result JSON file')
-    parser.add_argument('events_path', help='path to the events JSON file')
-    parser.add_argument("output_path", help="path to the output directory where frames will be saved")
-    args = parser.parse_args()
-
-    frames = load_frames(args.video_path)
-    rois = load_json(args.rois_path)
+def print_frames(video_path, rois_path, tr_path, events_path, output_path):
+    frames = load_frames(video_path)
+    rois = load_json(rois_path)
     tracking_result = TrackingResult()
-    tracking_result.load_from_json(frames, args.tracking_result_path)
-    events = load_json(args.events_path)
+    tracking_result.load_from_json(frames, tr_path)
+    events = load_json(events_path)
 
     bboxes_by_frame_index = {}
     bboxes_by_id = {}
@@ -78,5 +70,46 @@ if __name__ == '__main__':
         frame = render_frame_with_events(frames[idx], idx, rois, bboxes_by_frame_index[idx], events_in_frame, past_events)
         past_events += events_in_frame
         filename = 'frame-{}.png'.format(str(idx).zfill(n_digits))
-        save_image(frame, filename, args.output_path)
+        save_image(frame, filename, output_path)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('video_path', help='path to the video file')
+    parser.add_argument('rois_path', help='path to the rois JSON file')
+    parser.add_argument('tracking_result_path', help='path to a tracking result JSON file')
+    parser.add_argument('events_path', help='path to the events JSON file')
+    parser.add_argument("output_path", help="path to the output directory where frames will be saved")
+    args = parser.parse_args()
+
+    print_frames(args.video_path, args.rois_path, args.tracking_result_path, args.events_path, args.output_path)
+
+    # frames = load_frames(args.video_path)
+    # rois = load_json(args.rois_path)
+    # tracking_result = TrackingResult()
+    # tracking_result.load_from_json(frames, args.tracking_result_path)
+    # events = load_json(args.events_path)
+
+    # bboxes_by_frame_index = {}
+    # bboxes_by_id = {}
+    # for index in range(len(frames)):
+    #     bboxes_by_frame_index[index] = []
+    #     for bbox in tracking_result.frame_bundles[index].bboxes:
+    #         bboxes_by_id[bbox.id] = bbox
+    #         bboxes_by_frame_index[index].append(bbox)
+    # for track in tracking_result.tracks:
+    #     for frame_index, bbox, transition in track.steps:
+    #         if bbox.id not in bboxes_by_id:
+    #             bboxes_by_frame_index[frame_index].append(bbox)
+    #             bboxes_by_id[bbox.id] = bbox
+
+    # n_digits = len(str(len(frames)))
+    # events_per_frame = make_events_per_frame(events)
+
+    # past_events = []
+    # for idx in range(len(frames)):
+    #     events_in_frame = events_per_frame[idx] if idx in events_per_frame else []
+    #     frame = render_frame_with_events(frames[idx], idx, rois, bboxes_by_frame_index[idx], events_in_frame, past_events)
+    #     past_events += events_in_frame
+    #     filename = 'frame-{}.png'.format(str(idx).zfill(n_digits))
+    #     save_image(frame, filename, args.output_path)
 
