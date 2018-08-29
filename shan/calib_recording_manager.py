3f2f0e21aa978a999669b44e5e4d064ca55bd2d6
Gabriel Vieira <gluisvieira@gmail.com>
2018-08-29 14:52:31 -0300

    Remove old modules

--- shan/calib_recording_manager.py
+++
@@ -1,6 +0,0 @@
-"""
-    $ calib_recording_manager.py start_recording --duration 10
-    => created job and added to recorder worker queue
-    => recorder completed job 123, adding it to uploader queue
-    => uploader completed job 123
-"""
