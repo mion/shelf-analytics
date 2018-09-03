ba06771918bea9994401ee7155444c711a298119
Gabriel Vieira <gluisvieira@gmail.com>
2018-09-03 11:06:45 -0300

    Remove all visible files

--- scripts/.archive/viz_tracks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-TARGET_DIR=/Users/gvieira/shan/test
-rm -rf $TARGET_DIR/tracks
-mkdir $TARGET_DIR/tracks
-python scripts/visualize_tracks2.py ~/shan/video-33-p_06/video-33-p_06-fps-10.mp4 ~/shan/video-33-p_06/frames_retagged/tags.json /tmp/tracks.json $TARGET_DIR/tracks
--- scripts/_makevideo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIR_PATH=$1
-FPS=$2
-OUTPUT_NAME=$3
-cd $DIR_PATH
-ffmpeg -framerate $FPS -pattern_type glob -i "*.png" -c:v libx264 -r $FPS -pix_fmt yuv420p $OUTPUT_NAME
