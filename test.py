e7c326a55d9f50dab53754bcafa0f47906b9b79c
Gabriel Vieira <gluisvieira@gmail.com>
2018-09-04 13:51:14 -0300

    Add frame_splitting test

--- test.py
+++ test.py
@@ -14,8 +14,23 @@ def test_detection(ws_path):
 def test_event_extraction(ws_path):
     pass
 
-def test_frame_splitter(ws_path):
-    pass
+def test_frame_splitting(ws_path):
+    import uuid
+    from shan.core.frame_splitting import split_frames
+    EXT = 'png'
+    input_video_path = os.path.join(get_currendir(), 'test/fixture/frame_splitter/walked-single-fps-10.mp4')
+    output_dir_path = os.path.join(ws_path, 'frame_splitter_test_{}'.format(uuid.uuid4().hex))
+    os.mkdir(output_dir_path)
+    print(yellow('[*] Input video path: ') + input_video_path)
+    print(yellow('[*] Output dir path: ') + output_dir_path)
+    print(yellow('[*] Extension: ') + EXT)
+
+    success = split_frames(input_video_path, output_dir_path, EXT)
+
+    if success and os.path.exists(os.path.join(output_dir_path, 'frame-56.png')):
+        print(green('[*] TEST SUCCESSFUL'))
+    else:
+        print(red('[!] TEST FAILED'))
 
 def test_iaot(ws_path):
     pass
@@ -35,14 +50,15 @@ def test_transcoding(ws_path):
     print(yellow('[*] FPS: ') + str(FPS))
 
     success = transcode(input_video_path, output_video_path, FPS)
+
     if success and os.path.exists(output_video_path):
-        print(green('[*] TEST SUCCESFUL'))
+        print(green('[*] TEST SUCCESSFUL'))
     else:
         print(red('[!] TEST FAILED'))
 
 if __name__ == '__main__':
     argparse = argparse.ArgumentParser(description='A very simple and rudimentary integration test for all the code.')
-    argparse.add_argument('name', type=str, help="Name of the module to test: 'all', 'detection', 'event_extraction', 'frame_splitter', 'iaot', 'tracking' or 'transcoding'")
+    argparse.add_argument('name', type=str, help="Name of the module to test: 'all', 'detection', 'event_extraction', 'frame_splitting', 'iaot', 'tracking' or 'transcoding'")
     argparse.add_argument('-p', '--path', type=str, help="Path to the temporary directory where files will be created.")
     args = argparse.parse_args()
 
@@ -50,7 +66,7 @@ if __name__ == '__main__':
         'all': test_all,
         'detection': test_detection,
         'event_extraction': test_event_extraction,
-        'frame_splitter': test_frame_splitter,
+        'frame_splitting': test_frame_splitting,
         'iaot': test_iaot,
         'tracking': test_tracking,
         'transcoding': test_transcoding
