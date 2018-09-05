1dca32087a0a153a635d0353b821d4ff358a8c43
Gabriel Vieira <gluisvieira@gmail.com>
2018-09-05 06:13:57 -0300

    Refactor DEV_QUEUE_HOST const

--- configuration.py
+++ configuration.py
@@ -1,3 +1,5 @@
+DEV_QUEUE_HOST = 'localhost'
+
 configuration = {
     'dev': {
         'API_base_url': 'http://localhost:8000/shancms',
@@ -7,77 +9,77 @@ configuration = {
         'default_rois_path': '/Users/gvieira/code/toneto/shan/test/rois/v11s1.json',
         'workers': {
             'default': {
-                'QUEUE_HOST': 'localhost',
+                'QUEUE_HOST': DEV_QUEUE_HOST,
                 'QUEUE_NAME': 'default2',
                 'QUEUE_DURABLE': True,
                 'QUEUE_PREFETCH_COUNT': 1, # do not give more than one message to a worker at a time
                 'DELIVERY_MODE': 2 # make message persistent, for stronger guarantee of persistance see: https://www.rabbitmq.com/confirms.html
             },
             'db_saver': {
-                'QUEUE_HOST': 'localhost',
+                'QUEUE_HOST': DEV_QUEUE_HOST,
                 'QUEUE_NAME': 'db_saver1',
                 'QUEUE_DURABLE': True,
                 'QUEUE_PREFETCH_COUNT': 1, # do not give more than one message to a worker at a time
                 'DELIVERY_MODE': 2 # make message persistent, for stronger guarantee of persistance see: https://www.rabbitmq.com/confirms.html
             },
             'detector': {
-                'QUEUE_HOST': 'localhost',
+                'QUEUE_HOST': DEV_QUEUE_HOST,
                 'QUEUE_NAME': 'detector2',
                 'QUEUE_DURABLE': True,
                 'QUEUE_PREFETCH_COUNT': 1, # do not give more than one message to a worker at a time
                 'DELIVERY_MODE': 2 # make message persistent, for stronger guarantee of persistance see: https://www.rabbitmq.com/confirms.html
             },
             'downloader': {
-                'QUEUE_HOST': 'localhost',
+                'QUEUE_HOST': DEV_QUEUE_HOST,
                 'QUEUE_NAME': 'downloader1',
                 'QUEUE_DURABLE': True,
                 'QUEUE_PREFETCH_COUNT': 1, # do not give more than one message to a worker at a time
                 'DELIVERY_MODE': 2 # make message persistent, for stronger guarantee of persistance see: https://www.rabbitmq.com/confirms.html
             },
             'event_extractor': {
-                'QUEUE_HOST': 'localhost',
+                'QUEUE_HOST': DEV_QUEUE_HOST,
                 'QUEUE_NAME': 'event_extractor1',
                 'QUEUE_DURABLE': True,
                 'QUEUE_PREFETCH_COUNT': 1, # do not give more than one message to a worker at a time
                 'DELIVERY_MODE': 2 # make message persistent, for stronger guarantee of persistance see: https://www.rabbitmq.com/confirms.html
             },
             'evented_video_maker': {
-                'QUEUE_HOST': 'localhost',
+                'QUEUE_HOST': DEV_QUEUE_HOST,
                 'QUEUE_NAME': 'evented_video_maker1',
                 'QUEUE_DURABLE': True,
                 'QUEUE_PREFETCH_COUNT': 1, # do not give more than one message to a worker at a time
                 'DELIVERY_MODE': 2 # make message persistent, for stronger guarantee of persistance see: https://www.rabbitmq.com/confirms.html
             },
             'frame_splitter': {
-                'QUEUE_HOST': 'localhost',
+                'QUEUE_HOST': DEV_QUEUE_HOST,
                 'QUEUE_NAME': 'frame_splitter1',
                 'QUEUE_DURABLE': True,
                 'QUEUE_PREFETCH_COUNT': 1, # do not give more than one message to a worker at a time
                 'DELIVERY_MODE': 2 # make message persistent, for stronger guarantee of persistance see: https://www.rabbitmq.com/confirms.html
             },
             'recorder': {
-                'QUEUE_HOST': 'localhost',
+                'QUEUE_HOST': DEV_QUEUE_HOST,
                 'QUEUE_NAME': 'recorder1',
                 'QUEUE_DURABLE': True,
                 'QUEUE_PREFETCH_COUNT': 1, # do not give more than one message to a worker at a time
                 'DELIVERY_MODE': 2 # make message persistent, for stronger guarantee of persistance see: https://www.rabbitmq.com/confirms.html
             },
             'tracker': {
-                'QUEUE_HOST': 'localhost',
+                'QUEUE_HOST': DEV_QUEUE_HOST,
                 'QUEUE_NAME': 'tracker1',
                 'QUEUE_DURABLE': True,
                 'QUEUE_PREFETCH_COUNT': 1, # do not give more than one message to a worker at a time
                 'DELIVERY_MODE': 2 # make message persistent, for stronger guarantee of persistance see: https://www.rabbitmq.com/confirms.html
             },
             'transcoder': {
-                'QUEUE_HOST': 'localhost',
+                'QUEUE_HOST': DEV_QUEUE_HOST,
                 'QUEUE_NAME': 'transcoder1',
                 'QUEUE_DURABLE': True,
                 'QUEUE_PREFETCH_COUNT': 1, # do not give more than one message to a worker at a time
                 'DELIVERY_MODE': 2 # make message persistent, for stronger guarantee of persistance see: https://www.rabbitmq.com/confirms.html
             },
             'uploader': {
-                'QUEUE_HOST': 'localhost',
+                'QUEUE_HOST': DEV_QUEUE_HOST,
                 'QUEUE_NAME': 'uploader1',
                 'QUEUE_DURABLE': True,
                 'QUEUE_PREFETCH_COUNT': 1, # do not give more than one message to a worker at a time
