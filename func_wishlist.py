bfc0f9b6266ef8d9691c5aacb24359e5df11ba3c
Gabriel Vieira <gluisvieira@gmail.com>
2018-10-16 11:45:00 -0300

    Add load_video to func wishlist

--- func_wishlist.py
+++ func_wishlist.py
@@ -1,3 +1,27 @@
 from data_definitions import *
 
-p = Point(0, 0)
+def load_video(file_path):
+    """Loads a video file in memory using ``OpenCV``.
+
+    The video will be split into many frames, each a ``Numpy`` image, and they
+    will ALL be loaded in RAM memory.
+
+    Parameters
+    ----------
+    file_path : str
+        The full path of the video file to load.
+
+    Returns
+    -------
+    Video
+        A `Video` object containing the loaded frames and some other
+        properties.
+
+    Raises
+    ------
+    ValueError
+        If `file_path` is an empty string or `None`.
+    """
+    if not file_path:
+        raise ValueError('file_path must not be empty')
+    return Video()
