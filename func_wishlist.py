b3b44ff68bc41500bd90e745ec40b64ff7494e41
Gabriel Vieira <gluisvieira@gmail.com>
2018-10-16 12:31:03 -0300

    Add detect_humans to func wishlist

--- func_wishlist.py
+++ func_wishlist.py
@@ -1,5 +1,32 @@
 from data_definitions import *
 
+def detect_humans(video, config, params):
+    """Detect human beings that appear in the frames of a `Video` object.
+
+    Applies object detection, filtering out objects that are not human
+    beings.
+
+    Parameters
+    ----------
+    video : Video
+        The `Video` object containing the frames upon which object detection
+        will be applied, one by one.
+    config : dict
+        A dictionary specifying options such as the detection engine to be used
+        (e.g. `YOLO` or `MaskRCNN`).
+    params : dict
+        A dictionary specifying values that should be calibrated according to
+        the placement of the camera, and also that could be iterated upon
+        to increase overall accuracy.
+
+    Returns
+    -------
+    pdvideo : PostDetectionVideo
+        A `PostDetectionVideo` object that embodies the result of the
+        application of object detection to its frames.
+    """
+    return None
+
 def load_video(file_path):
     """Loads a video file in memory using ``OpenCV``.
 
