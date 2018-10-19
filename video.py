1c6e2ee123da7db7596db9479d56585ff714b7b8
Gabriel Vieira <gluisvieira@gmail.com>
2018-10-19 10:53:53 -0300

    Add docstring to video

--- video.py
+++ video.py
@@ -1,3 +1,8 @@
 class Video:
+    """
+    A video is a list of Frames, each representing an image, that was loaded
+    with OpenCV. It also contain properties such as the framerate (FPS), the
+    format (e.g.: mp4), the name of the file, etc.
+    """
     def __init__(self, frames=None):
         self.frames = [] if frames is None else frames
