eab48c970e5c4205be151a61e3c0a6c03a8196cc
Gabriel Vieira <gluisvieira@gmail.com>
2018-10-22 18:30:32 -0300

    Refactor test case

--- test_humantracking.py
+++ test_humantracking.py
@@ -4,11 +4,12 @@ from boundingbox import BBox
 from humantracking import find_bbox_to_snap
 
 class TestFindBBoxToSnap(unittest.TestCase):
-    def test_should_find_closest(self):
-        b1 = BBox(Point(25, 25), 200, 150)
-        b2 = BBox(Point(25, 100), 200, 150)
-        b3 = BBox(Point(25, 175), 200, 150)
-        bboxes = [b1, b2, b3]
+    def test_should_find_first_in_line(self):
+        bboxes = [
+            BBox(Point(25, 25), 200, 150),
+            BBox(Point(25, 100), 200, 150),
+            BBox(Point(25, 175), 200, 150)
+        ]
 
         closest_bbox_idx, closest_dist = find_bbox_to_snap(bboxes, 0, 1000)
 
