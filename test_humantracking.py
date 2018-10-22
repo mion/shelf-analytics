0664b8f65625fa13ffbe192fd87940dc2977e2a0
Gabriel Vieira <gluisvieira@gmail.com>
2018-10-22 18:28:38 -0300

    Add another test case to find_bbox_to_snap unit test

--- test_humantracking.py
+++ test_humantracking.py
@@ -15,5 +15,17 @@ class TestFindBBoxToSnap(unittest.TestCase):
         self.assertEqual(closest_bbox_idx, 1)
         self.assertEqual(closest_dist, 75)
 
+    def test_should_not_find_beyond_max(self):
+        bboxes = [
+            BBox(Point(25, 25), 200, 150),
+            BBox(Point(25, 175), 200, 150),
+            BBox(Point(25, 325), 200, 150)
+        ]
+
+        closest_bbox_idx, closest_dist = find_bbox_to_snap(bboxes, 1, 149)
+
+        self.assertEqual(closest_bbox_idx, None)
+        self.assertEqual(closest_dist, None)
+
 if __name__ == '__main__':
     unittest.main()
