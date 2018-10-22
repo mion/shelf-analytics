0710ec6cd3a9ad54fc6cd0c8917c426f6244326c
Gabriel Vieira <gluisvieira@gmail.com>
2018-10-22 18:27:04 -0300

    Refactor stub so unit test fails

--- humantracking.py
+++ humantracking.py
@@ -5,4 +5,4 @@ def track_humans(det_result, config, params):
     return []
 
 def find_bbox_to_snap(bboxes, base_bbox_index, max_snap_distance):
-    return (None, math.inf)
+    return (-1, math.inf)
