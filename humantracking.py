23a4ef0c6b2400771bf2858dc6b8a160a41b2751
Gabriel Vieira <gluisvieira@gmail.com>
2018-10-22 12:09:46 -0300

    Add find_bbox_to_snap func stub

---
+++ humantracking.py
@@ -0,0 +1,8 @@
+import math
+from boundingbox import BBox
+
+def track_humans(det_result, config, params):
+    return []
+
+def find_bbox_to_snap(bboxes, base_bbox_id, max_snap_distance):
+    return (None, math.inf)
