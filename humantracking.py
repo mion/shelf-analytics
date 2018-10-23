8124304442cfa904816d52b85e729eba2719bc2b
Gabriel Vieira <gluisvieira@gmail.com>
2018-10-23 12:53:52 -0300

    Refactor find_start, track humans; WIP find_all_tracks and find_some_track

--- humantracking.py
+++ humantracking.py
@@ -1,10 +1,60 @@
 import math
-from boundingbox import BBox
+from boundingbox import BBox, DetectedBBox
 
-def track_humans(det_result, config, params):
-    return []
+def track_humans(bboxes_per_frame, config, params):
+    """
+    dbboxes_per_frame is a list of pairs:
+        (frame, bboxes)
+    """
+    is_filtered = filter_bboxes(bboxes_per_frame)
+    tracks = find_all_tracks(bboxes_per_frame, is_filtered, params['MAX_TRACK_COUNT'])
+    return tracks
 
-def is_intersecting_any(bboxes, base_bbox_idx, min_intersec_area_perc):
+def filter_bboxes(bboxes_per_frame):
+    return bboxes_per_frame
+
+def find_all_tracks(bboxes_per_frame, is_filtered, max_track_count):
+    count = 0
+    tracks = []
+    parent_of = {}
+    while count < max_track_count:
+        track = find_some_track(bboxes_per_frame, is_filtered, parent_of)
+        count += 1
+        if track is not None:
+            tracks.append(track)
+        else:
+            break
+    return tracks
+
+def find_some_track(bboxes_per_frame, is_filtered, parent_of):
+    """
+    Returns None if no track found.
+    """
+    print("Searching for a bbox that is a good starting point...")
+    fr_idx, bbox_idx = find_start(bboxes_per_frame, is_filtered, parent_of, params)
+    if fr_idx is None:
+        print("None found. All humans have been tracked.")
+        return None
+    print("Found start bbox at frame index {:d} and bbox index {:d}".format(fr_idx, bbox_idx))
+    start_frame, start_bboxes = bboxes_per_frame[fr_idx]
+    start_bbox = start_bboxes[bbox_idx]
+    tracker = create_tracker(start_frame, start_bbox)
+    return None
+
+def create_tracker(frame, bbox):
+    # FIXME I removed the call to tracker.clear(), this probably brakes something.
+
+def find_start(bboxes_per_frame, is_filtered, parent_of, params):
+    for fr_idx, (frame, bboxes) in enumerate(bboxes_per_frame):
+        for bbox_idx, bbox in enumerate(bboxes):
+            unfiltered = (fr_idx, bbox_idx) not in is_filtered
+            untracked = (fr_idx, bbox_idx) not in parent_of
+            without_large_intersections = not is_intersecting_any(bboxes, bbox_idx, params['MIN_INTERSEC_AREA_PERC'])
+            if unfiltered and untracked and without_large_intersections:
+                return (fr_idx, bbox_idx)
+    return (None, None)
+
+def is_intersecting_any(bboxes, base_bbox_idx, intersec_area_perc_thresh):
     if not bboxes:
         raise RuntimeError("bboxes must not be empty")
     base_bbox = bboxes[base_bbox_idx]
@@ -17,7 +67,7 @@ def is_intersecting_any(bboxes, base_bbox_idx, min_intersec_area_perc):
         else:
             area_perc = intersec_area / base_bbox.area
             area_perc_vice_versa = intersec_area / bbox.area
-            if area_perc > min_intersec_area_perc or area_perc_vice_versa > min_intersec_area_perc:
+            if area_perc > intersec_area_perc_thresh or area_perc_vice_versa > intersec_area_perc_thresh:
                 return True
     return False
 
