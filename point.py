c0048c9187a481fc6659db46fb1bcc01673c969d
Gabriel Vieira <gluisvieira@gmail.com>
2018-10-29 18:37:35 -0300

    Make Point.add return a new Point instance, implement Point.normalize, Point.subtract and Point.normalized_direction

--- point.py
+++ point.py
@@ -9,10 +9,20 @@ class Point:
         dx = p.x - self.x
         dy = p.y - self.y
         return sqrt((dx * dx) + (dy * dy))
-    
+
     def copy(self):
         return Point(self.x, self.y)
-    
+
     def add(self, p):
-        self.x += p.x 
-        self.y += p.y
+        return Point(self.x + p.x, self.y + p.y)
+
+    def normalize(self):
+        length = self.distance_to(Point(0, 0))
+        return Point(self.x / length, self.y / length)
+
+    def subtract(self, p):
+        return Point(p.x - self.x, p.y - self.y)
+
+    def normalized_direction(self, p):
+        direction = self.subtract(p)
+        return direction.normalize()
