03e5092ed53bbb54f72e2271d0d27d864b87fc93
Gabriel Vieira <gluisvieira@gmail.com>
2018-10-31 13:55:30 -0300

    Rename interpolate args

--- humantracking.py
+++ humantracking.py
@@ -196,15 +196,15 @@ def find_some_track(bboxes_per_frame, is_filtered, params):
         curr_idx += 1
     return track
 
-def interpolate(last_bbox, curr_idx, target_idx, target_bbox):
-    n_hops = target_idx - curr_idx + 1
-    delta_per_hop = last_bbox.distance_to(target_bbox) / n_hops
-    dir_vec = last_bbox.center.normalized_direction(target_bbox.center)
-    interpol_orig = last_bbox.copy()
+def interpolate(start_bbox, start_idx, end_idx, end_bbox):
+    n_hops = end_idx - start_idx + 1
+    delta_per_hop = start_bbox.distance_to(end_bbox) / n_hops
+    dir_vec = start_bbox.center.normalized_direction(end_bbox.center)
+    interpol_orig = start_bbox.copy()
     steps = []
-    for idx in range(curr_idx, target_idx):
+    for idx in range(start_idx, end_idx):
         interpol_orig = interpol_orig.add(dir_vec.multiply(delta_per_hop))
-        interpol_bbox = BBox(interpol_orig, last_bbox.width, last_bbox.height)
+        interpol_bbox = BBox(interpol_orig, start_bbox.width, start_bbox.height)
         steps.append((idx, interpol_bbox, Transition.interpolated))
     return steps
 
