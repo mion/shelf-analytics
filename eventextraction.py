ac2a74c2f53046f425f240fcbc12f611e85771de
Gabriel Vieira <gluisvieira@gmail.com>
2018-11-13 11:43:14 -0300

    Add step_index param to Event and its subclasses

--- eventextraction.py
+++ eventextraction.py
@@ -16,21 +16,22 @@ class RegionOfInterest:
         self.event_types = event_types
 
 class Event:
-    def __init__(self, _type, roi_name):
+    def __init__(self, _type, roi_name, step_index):
         self.type = _type
         self.roi_name = roi_name
+        self.step_index = step_index
 
 class TraverseEvent(Event):
-    def __init__(self, roi_name):
-        super().__init__('traverse', roi_name)
+    def __init__(self, roi_name, step_index):
+        super().__init__('traverse', roi_name, step_index)
 
 class HoverEvent(Event):
-    def __init__(self, roi_name):
-        super().__init__('hover', roi_name)
+    def __init__(self, roi_name, step_index):
+        super().__init__('hover', roi_name, step_index)
 
 class InOutEvent(Event):
-    def __init__(self, roi_name):
-        super().__init__('in_out', roi_name)
+    def __init__(self, roi_name, step_index):
+        super().__init__('in_out', roi_name, step_index)
 
 def intersection_area_over_time(bboxes, roi_bbox):
     return [roi_bbox.intersection_area(bbox) for bbox in bboxes]
