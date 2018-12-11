679c5a4cd31cb855b743b7d9faac46ffa674c9a2
Gabriel Vieira <gluisvieira@gmail.com>
2018-12-11 15:39:58 -0300

    Remove unnecessary class

--- detectionresult.py
+++
@@ -1,7 +0,0 @@
-class DetectionResult:
-    """
-    The result of object detection.
-    """
-    def __init__(self, frames, dbboxes_by_frame_idx):
-        self.frames = frames
-        self.dbboxes_by_frame_idx = dbboxes_by_frame_idx
