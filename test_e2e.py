fa7f614a10f0d33315eb1997ffc39bf1e2f1ab8e
Gabriel Vieira <gluisvieira@gmail.com>
2018-12-13 17:05:35 -0300

    Add failing e2e test for traverse simple case

---
+++ test_e2e.py
@@ -0,0 +1,77 @@
+import unittest
+import json
+import cv2
+from point import Point
+from boundingbox import BBox, load_detected_bboxes_per_frame
+from humantracking import track_humans
+from eventextraction import RegionOfInterest as Roi, EventType, extract_events
+
+def load_json(path):
+    with open(path, "r") as file:
+        return json.loads(file.read())
+
+def read_frames_from_video(video):
+    is_first_frame = True
+    frames = []
+    while True:
+        ok, frame = video.read()
+        if not ok and is_first_frame:
+            return None
+        elif not ok and not is_first_frame:
+            break
+        else:
+            is_first_frame = False
+            frames.append(frame)
+    return frames
+
+def load_frames(path):
+    video = cv2.VideoCapture(path)
+    if not video.isOpened():
+        return None
+    return read_frames_from_video(video)
+
+class TraverseTest(unittest.TestCase):
+    def test_traverse_simple(self):
+        json_path = '/Users/gvieira/shan-test/traverse-simple.json'
+        raw_json = load_json(json_path)
+        det_bboxes_per_frame = load_detected_bboxes_per_frame(raw_json)
+        video_path = '/Users/gvieira/shan-test/traverse-simple.mp4'
+        frames = load_frames(video_path)
+        if len(frames) != len(det_bboxes_per_frame):
+            raise RuntimeError('frames and bboxes per frame lists have different length')
+        bboxes_per_frame = []
+        for idx, frame in enumerate(frames):
+            bboxes_per_frame.append((frame, det_bboxes_per_frame[idx]))
+        params = {
+            'MAX_TRACK_COUNT': 40,
+            'OPENCV_OBJ_TRACKER_TYPE': 'KCF',
+            'MIN_OBJ_DET_SCORE': 0.8,
+            'MIN_BBOX_AREA': 5000,
+            'MAX_BBOX_AREA': 15000,
+            'MAX_INTERSEC_AREA_PERC': 0.25,
+            'TRACKER_SUCCESS_MAX_SNAP_DISTANCE': 50,
+            'TRACKER_FAIL_MAX_SNAP_DISTANCE': 150,
+            'AVG_BBOX_VEL_MAX_BACK_HOPS': 5,
+            'LOOK_AHEAD_MAX_FRONT_HOPS': 5
+        }
+        tracks = track_humans(bboxes_per_frame, params)
+        self.assertEqual(len(tracks), 1)
+        self.assertGreater(len(tracks[0]), 40)
+        self.assertLess(tracks[0].get_step(0).frame_index, 70)
+        self.assertGreater(tracks[0].get_last_step().frame_index, 70)
+        rois = [
+            Roi('aisle', BBox(Point(176, 125), 219, 120), [EventType.traverse])
+        ]
+        bboxes_per_track = [track.get_bboxes() for track in tracks]
+        params_for_event_type = {
+            EventType.traverse: {'min_duration': 20, 'min_area': 10000}
+        }
+        events = extract_events(bboxes_per_track, rois, params_for_event_type)
+        self.assertEqual(len(events), 1)
+        self.assertEqual(events[0].type, EventType.traverse)
+        self.assertEqual(events[0].roi_name, 'aisle')
+        self.assertGreater(events[0].index, 50)
+        self.assertLess(events[0].index, 90)
+
+if __name__ == '__main__':
+    unittest.main()
